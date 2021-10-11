4b473f3adef25231de7f340bb94470e505d7dbe1
Matthias Schweikardt <Matthias.Schweikardt@reutlingen-university.de>
2021-10-11 12:15:13 +0200

    test environments

---
+++ src/test/java/edlab/eda/ace/AnalogCircuitEnvironmentTest.java
@@ -0,0 +1,97 @@
+package edlab.eda.ace;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.nio.file.Files;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+class AnalogCircuitEnvironmentTest {
+
+  @Test
+  void test() {
+
+    File resourcesFile = new File("resource");
+
+    String[] directories = resourcesFile.list(new FilenameFilter() {
+      @Override
+      public boolean accept(File current, String name) {
+        return new File(current, name).isDirectory();
+      }
+    });
+
+    File directoryFile;
+    File subDirectoryFile;
+    File jsonFile;
+    String[] subDirectories;
+    JSONObject jsonObj;
+
+    for (String directory : directories) {
+
+      directoryFile = new File(resourcesFile, directory);
+
+      subDirectories = directoryFile.list(new FilenameFilter() {
+        @Override
+        public boolean accept(File current, String name) {
+          return new File(current, name).isDirectory();
+        }
+      });
+
+      for (String subDirectory : subDirectories) {
+
+        subDirectoryFile = new File(directoryFile, subDirectory);
+
+        if (subDirectoryFile.isDirectory()) {
+
+          jsonFile = new File(subDirectoryFile,
+              AnalogCircuitEnvironment.JSON_FILE_NAME);
+
+          if (jsonFile.exists()) {
+
+            try {
+
+              jsonObj = new JSONObject(
+                  new String(Files.readAllBytes(jsonFile.toPath())));
+
+              AnalogCircuitEnvironment env = null;
+
+              switch (jsonObj.getString("environment")) {
+
+              case "SingleEndedOpampEnvironment":
+                env = SingleEndedOpampEnvironment.get(
+                    resourcesFile.getAbsoluteFile().toString(),
+                    subDirectoryFile.getAbsoluteFile().toString(),
+                    new String[] { jsonObj.getString("models") });
+                break;
+
+              default:
+                fail("No environment \"" + jsonObj.getString("environment")
+                    + "\" is available\n");
+                break;
+              }
+
+              if (env != null) {
+                
+                env.simulate();
+                
+                env.stop();
+
+              } else {
+                fail("Cannot create environment for \""
+                    + subDirectoryFile.getAbsolutePath() + "\"");
+              }
+
+            } catch (Exception e) {
+              fail("Unable to read JSON \"" + jsonFile.getAbsolutePath()
+                  + "\"\n\t" + e.getMessage());
+            }
+
+          }
+        }
+      }
+    }
+  }
+}
